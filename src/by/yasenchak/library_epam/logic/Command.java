94accfefb7bd8291aa072f52696406eb489d8868
Ясенчак Александр <Yasenchak@tenzor.local>
2018-12-13 17:33:04 +0300

    deleted other files

--- src/by/yasenchak/library_epam/logic/Command.java
+++
@@ -1,7 +0,0 @@
-package by.yasenchak.library_epam.logic;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface Command {
-    String execute(HttpServletRequest request);
-}
--- src/by/yasenchak/library_epam/service/ClientService.java
+++
@@ -1,9 +0,0 @@
-package by.yasenchak.library_epam.service;
-
-import by.yasenchak.library_epam.entity.User;
-import by.yasenchak.library_epam.exception.ServiceException;
-
-public interface ClientService {
-    User signIn(String login, String password) throws ServiceException;
-    void registration(User user) throws ServiceException;
-}
